f897d70f84ebbd07a39f3da4974e56bf0ffefd30
Thiago Colbert <th.colbert@gmail.com>
2016-05-30 00:07:29 -0300

    Corrigindo erros na integração do Jetty e Jersey com Weld

--- mychart2-web-jetty/src/main/java/br/com/colbert/mychart/jetty/Main.java
+++ mychart2-web-jetty/src/main/java/br/com/colbert/mychart/jetty/Main.java
@@ -1,13 +1,17 @@
 package br.com.colbert.mychart.jetty;
 
 import java.io.File;
+import java.net.URI;
 import java.util.Arrays;
+
+import javax.ws.rs.core.UriBuilder;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AllowSymLinkAliasChecker;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.*;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
 /**
  * 
@@ -26,29 +30,37 @@ public final class Main {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
-
-		WebAppContext webApp = new WebAppContext();
-		webApp.setContextPath("/mychart");
-
-		File warFile = new File("../mychart2-web/target/mychart2");
-		webApp.setWar(warFile.getAbsolutePath());
-		webApp.setResourceBase(warFile.getAbsolutePath());
-
-		webApp.setConfigurationClasses(Arrays.asList("org.eclipse.jetty.webapp.WebXmlConfiguration"));
-		webApp.addAliasCheck(new AllowSymLinkAliasChecker());
-
-		WebAppClassLoader classLoader = new WebAppClassLoader(webApp);
-		classLoader.addClassPath("../mychart2-web/target/mychart2/WEB-INF/classes");
-		Resource jars = Resource.newResource("../mychart2-web/target/mychart2/WEB-INF/lib");
-		classLoader.addJars(jars);
-		webApp.setClassLoader(classLoader);
+		/*System.setProperty("com.sun.jersey.server.impl.cdi.lookupExtensionInBeanManager", "true");
 
 		server.setHandler(webApp);
 
 		server.start();
 		// server.dumpStdErr();
 		server.setStopAtShutdown(true);
-		server.join();
+		server.join();*/
+		
+		WebAppContext webApp = new WebAppContext();
+		webApp.setContextPath("/mychart");
+		
+		File warFile = new File("../mychart2-web/target/mychart2");
+		webApp.setWar(warFile.getAbsolutePath());
+		webApp.setResourceBase(warFile.getAbsolutePath());
+		
+		webApp.setConfigurationClasses(Arrays.asList("org.eclipse.jetty.webapp.WebXmlConfiguration"));
+		webApp.addAliasCheck(new AllowSymLinkAliasChecker());
+		
+		WebAppClassLoader classLoader = new WebAppClassLoader(webApp);
+		classLoader.addClassPath("../mychart2-web/target/mychart2/WEB-INF/classes");
+		Resource jars = Resource.newResource("../mychart2-web/target/mychart2/WEB-INF/lib");
+		classLoader.addJars(jars);
+
+		ResourceConfig application = new ResourceConfig().setClassLoader(classLoader);
+		
+		URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).build();
+		ResourceConfig config = ResourceConfig.forApplication(application);
+
+	    Server server = JettyHttpContainerFactory.createServer(baseUri, config);
+
+	    server.join();
 	}
 }
---
+++ mychart2-web/src/test/java/br/com/colbert/mychart/application/artista/ArtistaResourceIT.java
@@ -0,0 +1,23 @@
+package br.com.colbert.mychart.application.artista;
+
+import static org.junit.Assert.fail;
+
+import org.junit.*;
+
+import br.com.colbert.mychart.test.AbstractIT;
+
+/**
+ * Testes de integração da {@link ArtistaResource}.
+ * 
+ * @author ThiagoColbert
+ * @since 29 de mai de 2016
+ */
+@Ignore
+public class ArtistaResourceIT extends AbstractIT {
+
+	@Test
+	public void testRecuperarTodos() {
+		fail("Not yet implemented");
+	}
+
+}
